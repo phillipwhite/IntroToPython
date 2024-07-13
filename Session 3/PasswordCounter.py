6deab606c4df3b8a16aa2072f716555793377485
phillipwhite <philwhiteuk1@gmail.com>
2024-07-13 10:17:04 +0100

    Added files for Sessions 3

--- Session 3/PasswordCounter.py
+++ Session 3/PasswordCounter.py
@@ -1,9 +1,13 @@
 PASSWORD = 'super_password123'
 password_entered = ''
+counter = 0
 
-while password_entered != PASSWORD:
+while password_entered != PASSWORD and counter < 3:
     password_entered = input("Enter the password : ")
+    counter += 1
     if password_entered == PASSWORD:
         print("Access Granted!")
     else:
-        print("Forbidden!")
+        print("Forbidden! That was attempt number", counter)
+
+print("Forbidden after three failed attempts!")
