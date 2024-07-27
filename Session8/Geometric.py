9b0ba7766b15616b1cc328222bf8520005abdbee
phillipwhite <philwhiteuk1@gmail.com>
2024-07-27 13:39:10 +0100

    Intel

--- Session8/Geometric.py
+++ Session8/Geometric.py
@@ -7,7 +7,21 @@ class GeometricObject:
         return self.colour
 
     def setColour(self, colour:str):
+        self.colour = colour
+
+
+class Circle(GeometricObject):
+
+    def __init__(self, colour:str = "white", filled:bool = False,
+                 radius:float = 1):
+        super.__init__(colour, filled)
+        self.radius = radius
+
 
 
 ob1 = GeometricObject()
 ob2 = GeometricObject("blue", True)
+
+circ1 = Circle("blue", True, 3)
+
+
---
+++ Session8/StockPrice.py
@@ -0,0 +1,36 @@
+class Stock:
+
+    def __init__(self, symbol: str, name: str,
+                 previousClosingPrice: float = 0,
+                 currentPrice: float = 0):
+        self.__symbol = symbol
+        self.__name = name
+        self.__previousClosingPrice = previousClosingPrice
+        self.__currentPrice = currentPrice
+
+    def getSymbol(self):
+        return self.__symbol
+
+    def getName(self):
+        return self.__name
+
+    def getPreviousPrice(self):
+        return self.__previousClosingPrice
+
+    def setPreviousPrice(self, previousClosingPrice):
+        self.__previousClosingPrice = previousClosingPrice
+
+    def getCurrentPrice(self):
+        return self.__currentPrice
+
+    def setCurrentPrice(self, currentPrice):
+        self.__currentPrice = currentPrice
+
+
+    def getChangePercent(self):
+        return (((self.__currentPrice - self.__previousClosingPrice) \
+            / self.__previousClosingPrice) - 1) * 100
+
+
+intelStock = Stock("INTC", "Intel Corporation",
+                   20.5, 20.35)
